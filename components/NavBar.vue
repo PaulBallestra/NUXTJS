11f1cacff341db728dc6c41ac0c0f0d0393f020c
PaulBallestra <paulballestra@yahoo.fr>
2021-06-07 12:39:28 +0200

    Fixed navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/jsp">Plus tard</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+
+  export default {}
+
+</script>
+
+<style scoped>
+  nav {
+    padding: 0 1rem;
+  }
+
+  nav ul {
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+  }
+
+  nav ul li {
+    padding: 1rem;
+    font-size: 1.2rem;
+  }
+</style>
---
+++ components/QuatreSansQuatre.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div class="container-fluid">
+
+    <div class="text-center">
+      <h1> Page Introuvable - 404 </h1>
+      <NuxtLink to="/"> Page d'accueil </NuxtLink>
+    </div>
+
+
+  </div>
+
+</template>
+
+<script>
+
+  export default {
+  }
+
+</script>
